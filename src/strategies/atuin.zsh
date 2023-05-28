6a793615135fbf6e44863c3bfc23dd38eed9352d
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2023-05-28 21:04:43 +0200

    Add strategy for atuin

---
+++ src/strategies/atuin.zsh
@@ -0,0 +1,30 @@
+
+#--------------------------------------------------------------------#
+# History Suggestion Strategy (atuin backend)						 #
+#--------------------------------------------------------------------#
+# Suggests the most recent history item that matches the given
+# prefix.
+#
+
+_zsh_autosuggest_strategy_atuin() {
+	# Reset options to defaults and enable LOCAL_OPTIONS
+	emulate -L zsh
+
+	# Enable globbing flags so that we can use (#m) and (x~y) glob operator
+	setopt EXTENDED_GLOB
+
+	# Escape backslashes and all of the glob operators so we can use
+	# this string as a pattern to search the $history associative array.
+	# - (#m) globbing flag enables setting references for match data
+	# TODO: Use (b) flag when we can drop support for zsh older than v5.0.8
+	local prefix="${1//(#m)[\\*?[\]<>()|^~#]/\\$MATCH}"
+
+	# Get the history items that match the prefix, excluding those that match
+	# the ignore pattern
+	local pattern="$prefix*"
+	if [[ -n $ZSH_AUTOSUGGEST_HISTORY_IGNORE ]]; then
+		pattern="($pattern)~($ZSH_AUTOSUGGEST_HISTORY_IGNORE)"
+	fi
+
+	typeset -g suggestion="$(atuin search --search-mode prefix --format {command} --limit 1 ${pattern})"
+}
